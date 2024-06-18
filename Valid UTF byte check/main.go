9b812d58f6f331944002ca34dfb7ee9f45b2835b
rishav-karanjit <karanjitrishav4@gmail.com>
2024-06-18 15:56:14 -0700

    Add one more approach

--- Valid UTF byte check/main.go
+++ Valid UTF byte check/main.go
@@ -9,4 +9,14 @@ func main() {
 	a := string(uint8(255))
 	fmt.Println(a)
 	fmt.Println(utf8.ValidString(a))
+
+	// Create the byte slice
+	invalidBytes := []byte{uint8(255)}
+	stringinvalidBytes := string(invalidBytes)
+	// Check if the byte slice is a valid UTF-8 sequence
+	if utf8.ValidString(stringinvalidBytes) {
+		fmt.Println("The byte sequence is valid UTF-8")
+	} else {
+		fmt.Println("The byte sequence is not valid UTF-8")
+	}
 }
