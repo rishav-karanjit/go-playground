7b70f6377a1accfae0a9ef7008ec619f71a2b16c
rishav-karanjit <karanjitrishav4@gmail.com>
2025-03-18 22:09:45 -0700

    kms go sdk v2

--- aws-sdk-go/kms/main.go
+++ aws-sdk-go/kms/main.go
@@ -2,106 +2,71 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"reflect"
+	"log"
+	"os"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/kms"
-	"github.com/aws/aws-sdk-go-v2/service/kms/types"
-	awsv1 "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	kmsv1 "github.com/aws/aws-sdk-go/service/kms"
-	"github.com/aws/smithy-go"
 )
 
 func main() {
-	fmt.Println("\n v2 client \n")
-	mainv2()
-	// fmt.Println("\n v1 client \n")
-	// mainv1()
-}
-func mainv2() {
-	// Load AWS credentials and configuration from environment variables or shared credentials file
+	// Load AWS configuration
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to load AWS configuration:", err)
-		return
+		log.Fatalf("Failed to load AWS configuration: %v", err)
 	}
 
-	// Create KMS service client
-	svc := kms.NewFromConfig(cfg)
+	// Create KMS client
+	kmsClient := kms.NewFromConfig(cfg)
 
-	// Plaintext data to encrypt
+	// Get KMS key ARN from environment variable
+	keyID := os.Getenv("key1")
+	if keyID == "" {
+		log.Fatal("AWS_KMS_KEY_ID environment variable is required")
+	}
+
+	// Original data to encrypt
 	plaintext := []byte("Hello, World!")
-	key := "arn:aws:kms:us-west-2:992382771485:key/ad1d7ff3-79f3-40f4-b31d-7be4d9c8b3cc"
+
 	// Encrypt the data
-	input := &kms.EncryptInput{
-		KeyId:     &key, // Replace with your KMS key ID
-		Plaintext: plaintext,
-	}
-
-	result, err := svc.Encrypt(context.TODO(), input)
+	ciphertext, err := encryptData(kmsClient, keyID, plaintext)
 	if err != nil {
-		var ae smithy.APIError
-		if errors.As(err, &ae) {
-			fmt.Printf("code: %s,\n message: %s,\n fault: %s", ae.ErrorCode(), ae.ErrorMessage(), ae.ErrorFault().String())
-		}
-		var oe *smithy.OperationError
-		if errors.As(err, &oe) {
-			fmt.Printf("failed to call service: %s, operation: %s, error: %v", oe.Service(), oe.Operation(), oe.Unwrap())
-		}
-
-		// var errr *types.Tag
-		var err1 *types.CloudHsmClusterInUseException
-		var err2 *types.NotFoundException
-		switch {
-		case errors.As(err, &err1):
-			fmt.Println("Error is of type Error1:", err1)
-		case errors.As(err, &err2):
-			fmt.Println(reflect.TypeOf(err2))
-			fmt.Println("Error is of type Error2:", err2)
-		}
-		// var oe *types.CloudHsmClusterInUseException
-		// if errors.As(err, &oe) {
-		// 	fmt.Println(reflect.TypeOf(oe))
-		// 	// fmt.Println("failed to call service: %s, operation: %s, error: %v", oe.Service(), oe.Operation(), oe.Unwrap())
-		// 	// fmt.Println(reflect.TypeOf(oe.Unwrap()))
-		// }
-		// fmt.Println(reflect.TypeOf(err))
-		// fmt.Println(reflect.TypeOf(err.(*smithy.OperationError).Err))
-		// fmt.Println("Failed to encrypt data:", err)
-		return
+		log.Fatalf("Failed to encrypt data: %v", err)
 	}
+	fmt.Printf("Encrypted data: %x\n", ciphertext)
 
-	fmt.Printf("Encrypted data: %x\n", result.CiphertextBlob)
+	// Decrypt the data
+	decryptedText, err := decryptData(kmsClient, ciphertext)
+	if err != nil {
+		log.Fatalf("Failed to decrypt data: %v", err)
+	}
+	fmt.Printf("Decrypted data: %s\n", string(decryptedText))
 }
 
-func mainv1() {
-	// Load AWS credentials and configuration from environment variables or shared credentials file
-	sess := session.Must(session.NewSession(&awsv1.Config{
-		LogLevel: awsv1.LogLevel(awsv1.LogDebugWithSigning),
-	}))
-
-	// Create KMS service client
-	svc := kmsv1.New(sess)
-
-	// Plaintext data to encrypt
-	plaintext := []byte("Hello, World!")
-
-	// Encrypt the data
-	input := &kmsv1.EncryptInput{
-		KeyId:     aws.String("arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7g"), // Replace with your KMS key ID
+func encryptData(client *kms.Client, keyID string, plaintext []byte) ([]byte, error) {
+	input := &kms.EncryptInput{
+		KeyId:     &keyID,
 		Plaintext: plaintext,
 	}
 
-	result, err := svc.Encrypt(input)
+	result, err := client.Encrypt(context.TODO(), input)
 	if err != nil {
-		// fmt.Println(reflect.TypeOf(err))
-		// fmt.Println("Failed to encrypt data:", err)
-		return
+		return nil, fmt.Errorf("encryption error: %w", err)
 	}
 
-	fmt.Printf("Encrypted data: %x\n", result.CiphertextBlob)
+	return result.CiphertextBlob, nil
+}
+
+func decryptData(client *kms.Client, ciphertext []byte) ([]byte, error) {
+	input := &kms.DecryptInput{
+		CiphertextBlob: ciphertext,
+	}
+
+	result, err := client.Decrypt(context.TODO(), input)
+	if err != nil {
+		return nil, fmt.Errorf("decryption error: %w", err)
+	}
+
+	return result.Plaintext, nil
 }
